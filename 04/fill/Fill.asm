29dc4f956945ed7b8254f095e92fbc4c227baa8c
Ryan Glassett <rglassett@gmail.com>
2017-07-21 12:39:21 -0700

    Add solutions for project 04

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,67 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(Main)
+
+// draw all addresses between SCREEN and 24576 (KEYBOARD)
+@24576
+D=A
+@SCREEN
+D=D-A
+@drawsRemaining
+M=D
+
+// start the cursor at SCREEN
+@SCREEN
+D=A
+@cursor
+M=D
+
+// check keyboard input and branch to either Fill or Clear
+@24576
+D=M
+@Fill
+D;JGT
+@Clear
+D;JEQ
+
+(Fill)
+@fillValue
+M=-1
+
+@Draw
+0;JMP
+
+(Clear)
+@fillValue
+M=0
+
+@Draw
+0;JMP
+
+(Draw)
+@fillValue
+D=M
+
+@cursor
+A=M
+M=D
+@cursor
+M=M+1
+
+@drawsRemaining
+M=M-1
+D=M
+
+@Draw
+D;JGT
+
+@Main
+0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,45 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// initialize product to 0
+@R2
+M=0
+
+// terminate if any inputs are 0
+@R0
+D=M
+@End
+D;JEQ
+
+@R1
+D=M
+@End
+D;JEQ
+
+// add R0 to R2 for R1 repetitions
+@R1
+D=M
+@additionsRemaining
+M=D
+
+(Loop)
+
+@additionsRemaining
+D=M
+@End
+D;JEQ
+
+@R0
+D=M
+@R2
+M=D+M
+
+@additionsRemaining
+M=M-1
+@Loop
+0;JMP
+
+// infinite loop to signal the program is done
+(End)
+@End
+0;JMP
